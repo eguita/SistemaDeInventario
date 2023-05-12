c4265637f3f5277db3c13c25859aa0ec9e0e9ed0
Edurvi <edgui-vm@hotmail.com>
2023-05-12 12:49:49 -0500

    DMarca

--- Sistema/Sistema.Datos/DMarca.cs
+++ Sistema/Sistema.Datos/DMarca.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,36 +13,188 @@ namespace Sistema.Datos
     {
         public DataTable Listar()
         {
+            SqlDataReader Resultado; // Proporociona leer una secuencia de filas en sql server
+            DataTable Tabla = new DataTable(); //DataTable representa una tabla en memoria
+            SqlConnection SqlCon = new SqlConnection(); //Variable que establece la conexion a la BD
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("MARCA_LISTAR", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
 
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
         }
         public DataTable Buscar(String Valor)
         {
 
+            SqlDataReader Resultado; // Proporociona leer una secuencia de filas en sql server
+            DataTable Tabla = new DataTable(); //DataTable representa una tabla en memoria
+            SqlConnection SqlCon = new SqlConnection(); //Variable que establece la conexion a la BD
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("MARCA_BUSCAR", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@VALOR", SqlDbType.VarChar).Value = Valor;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
         }
 
-        public string Insertar(Categoria Obj)
+        public string Insertar(Marca Obj)
         {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion(); // Conexion a la BD
+                SqlCommand Comando = new SqlCommand("MARCA_INSERTAR", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@NOMBRE", SqlDbType.VarChar).Value = Obj.Nombre;
+                Comando.Parameters.Add("@DESCRIPCION", SqlDbType.VarChar).Value = Obj.Descripcion;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo ingresar el registro";
 
+
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
         }
 
-        public string Actualizar(Categoria Obj)
+        public string Actualizar(Marca Obj)
         {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion(); // Conexion a la BD
+                SqlCommand Comando = new SqlCommand("MARCA_ACTUALIZAR", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@ID_MARCA", SqlDbType.Int).Value = Obj.IdMarca;
+                Comando.Parameters.Add("@NOMBRE", SqlDbType.VarChar).Value = Obj.Nombre;
+                Comando.Parameters.Add("@DESCRIPCION", SqlDbType.VarChar).Value = Obj.Descripcion;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo actualizar el registro";
 
+
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
         }
 
         public string Eliminar(int Id)
         {
 
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion(); // Conexion a la BD
+                SqlCommand Comando = new SqlCommand("MARCA_ELIMINAR", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@ID_MODELO", SqlDbType.Int).Value = Id;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo Eliminar el registro";
+
+
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
         }
 
         public string Activar(int Id)
         {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion(); // Conexion a la BD
+                SqlCommand Comando = new SqlCommand("MARCA_ACTIVAR", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@ID_MARCA", SqlDbType.Int).Value = Id;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo Activar el registro";
 
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
         }
 
-        public Desactivar(int Id)
+        public string Desactivar(int Id)
         {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion(); // Conexion a la BD
+                SqlCommand Comando = new SqlCommand("MARCA_DESACTIVAR", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@ID_MARCA", SqlDbType.Int).Value = Id;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo Desactivar    el registro";
 
+
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
         }
     }
 }
